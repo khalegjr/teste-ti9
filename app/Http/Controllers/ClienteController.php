fe69e80fa6a8bbaa600913ecf112cd20217945e9
Khaled <khalegjr@gmail.com>
2023-03-05 15:46:32 -0300

    implementa listagem de todos os clientes

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cliente;
 use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
-    //
+    public function listaPedidos()
+    {
+        $clientes = Cliente::all();
+
+        return view('welcome', compact('clientes'));
+    }
 }
