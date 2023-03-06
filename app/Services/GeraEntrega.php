32192cc530524c53a7a016b1076d93c9c092b1dd
Khaled <khalegjr@gmail.com>
2023-03-06 19:45:57 -0300

    refac: Ajusta serviço para gerar entrega

--- app/Services/GeraEntrega.php
+++ app/Services/GeraEntrega.php
@@ -11,10 +11,61 @@ class GeraEntrega
 
     public function gerarEntrega()
     {
-        return [
+        return (object) [
             "normal" => $this->montarPrimeiraParte(),
             "especial" => $this->montarEspecial(),
         ];
+    }
+
+    private function montarPrimeiraParte() {
+        $clientes = Cliente::with([
+            'produtos' => function ($query) {
+            $query->where('peso', '<', 300);
+        }])
+            ->get()
+            ->groupBy('uf')
+            ->map(function ($group) {
+                $this->pesoTotal = 0;
+                return [
+                    'uf' => $group->first()->uf,
+                    'caminhao' => $this->somarCaminhoes(),
+                    'clientes' => $group->map(function ($cliente) {
+                        return $this->montarCliente($cliente);
+                    })->reject(function ($cliente) {
+                        return $cliente['produtos']->isEmpty();
+                    }),
+                    'peso_total' => number_format($this->pesoTotal, 3, ',', '.')
+                ];
+            })->reject(function ($group) {
+                return $group['clientes']->isEmpty();
+        })->values();
+
+        return $clientes->sortBy('uf');
+    }
+
+    private function montarEspecial()
+    {
+        $clientes = Cliente::whereHas('produtos', function($query) {
+            $query->where('peso', '>=', 300);
+        })->with([
+            'produtos' => function ($query) {
+            $query->where('peso', '>=', 300);
+        }])
+            ->get();
+
+        $clientes->sortBy('uf');
+
+        $clientes = $clientes->map(function ($cliente) {
+            $this->pesoTotal = 0;
+            return [
+                'uf' => $cliente->uf,
+                'caminhao' => $this->somarCaminhoes(),
+                'clientes' => [$this->montarCliente($cliente)],
+                'peso_total' => number_format($this->pesoTotal, 3, ',', '.'),
+            ];
+        });
+
+        return $clientes->sortBy('uf');
     }
 
     private function montarCliente($cliente)
@@ -37,59 +88,6 @@ class GeraEntrega
             'nome' => $produto->nome,
             'peso' => $produto->peso
         ];
-    }
-
-    private function montarPrimeiraParte() {
-        $clientes = Cliente::with([
-            'produtos' => function ($query) {
-            $query->where('peso', '<', 300);
-        }])
-            ->get()
-            ->groupBy('uf')
-            ->map(function ($group) {
-                $this->pesoTotal = 0;
-                return [
-                    'uf' => $group->first()->uf,
-                    'caminhao' => $this->somarCaminhoes(),
-                    'clientes' => $group->map(function ($cliente) {
-                        return $this->montarCliente($cliente);
-                    })->reject(function ($cliente) {
-                        return $cliente['produtos']->isEmpty();
-                    }),
-                    'peso_total' => $this->pesoTotal
-                ];
-            })->reject(function ($group) {
-                return $group['clientes']->isEmpty();
-        })->values();
-
-        return $clientes->sortBy('uf');
-    }
-
-    private function montarEspecial()
-    {
-        $clientes = Cliente::with([
-            'produtos' => function ($query) {
-            $query->where('peso', '>=', 300);
-        }])
-            ->get()
-            ->groupBy('uf')
-            ->map(function ($group) {
-                $this->pesoTotal = 0;
-                return [
-                    'uf' => $group->first()->uf,
-                    'caminhao' => $this->somarCaminhoes(),
-                    'clientes' => $group->map(function ($cliente) {
-                        return $this->montarCliente($cliente);
-                    })->reject(function ($cliente) {
-                        return $cliente['produtos']->isEmpty();
-                    }),
-                    'peso_total' => $this->pesoTotal
-                ];
-            })->reject(function ($group) {
-                return $group['clientes']->isEmpty();
-        })->values();
-
-        return $clientes->sortBy('uf');
     }
 
     private function somarCaminhoes(): int
